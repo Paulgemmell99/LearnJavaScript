fb2e72d7d4db4b9270284b6ac58021a0d9fc9f6c
Paul Gemmell <Paulgemmell91@gmail.com>
2016-12-12 15:52:14 +0000

    First pass innit

---
+++ public/javascript/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by davem on 27/11/2016.
+ */
+'use strict';
+var learnjavascript={};
+
+learnjavascript.showView = function (hash){
+    var questionView =$('<div class = "question-view">').text('Arriving soon!');
+    $('view-container').empty().append(questionView);
+
+}
+
+
+
+
